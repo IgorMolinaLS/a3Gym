e23d39d2f2fe17f29b1688bb1ce243f3edf70c2c
Igor Molina <igoramil452@gmai.com>
2023-10-12 11:25:39 -0300

    Criação da WelcomeScreen

---
+++ pages/Welcome.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View } from "react-native";
+import { Text } from "react-native-svg";
+
+const Welcome = () => {
+  return (
+    <View>
+      <Text>Welcome!</Text>
+    </View>
+  );
+};
+
+export default Welcome;
